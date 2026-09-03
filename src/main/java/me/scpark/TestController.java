19ddc4113095228e287d03c470c2b554fc3339c3
cy123 <cy762716603@gmail.com>
2026-09-03 16:30:01 +0900

    xie le geng duo de yao qiu wang zhan Implemen

--- src/main/java/me/scpark/TestController.java
+++ src/main/java/me/scpark/TestController.java
@@ -1,7 +1,13 @@
 package me.scpark;
 
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 
@@ -14,5 +20,22 @@ public class TestController {
     public String hi(){
         return ("안녕?'http://localhost:8080/hi' 에 대한 응답입니다.");
     }
+    @PostMapping ("/test")
+    public String deleteTest(){
+        return ("안녕?'http://localhost:8080/test' 에 대한 응답입니다.");
+    }
+    @PatchMapping ("/test")
+    public String patch(){
+        return ("안녕?'http://localhost:8080/test' 에 대한 응답입니다.");
+    }
+    @PutMapping ("/test")
+    public String Put(){
+        return ("안녕?'http://localhost:8080/test' 에 대한 응답입니다.");
+    }
+    @DeleteMapping ("/test")
+    public String Delete(){
+        return ("안녕?'http://localhost:8080/test' 에 대한 응답입니다.");
+    }
+
 
 }
